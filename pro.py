fa6cdec6c78e4536cbb191a124899c867b1ba30d
kaj-al <kt2519033@gmail.com>
2026-03-20 13:08:27 +0530

    create chat part with ui

--- pro.py
+++ pro.py
@@ -3,20 +3,33 @@ from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain.agents import create_agent
+import streamlit as st
 
 load_dotenv()
 
 model = ChatGroq(model="openai/gpt-oss-20b")
 search = GoogleSerperAPIWrapper()
 tools = search.run
-checkpointer = InMemorySaver()
 
-agent = create_agent(model=model,tools=[tools],checkpointer=checkpointer,system_prompt="You are an expert agent and search answers for any questions on Google.")
+if "memory" not in st.session_state:
+    st.session_state.memory = InMemorySaver()
+    st.session_state.history = []
 
-while True:
-    query = input("user: ")
-    if query.lower() == ["quit","bye"]:
-        break
+agent = create_agent(model=model,tools=[tools],checkpointer=st.session_state.memory,system_prompt="You are an expert agent and search answers for any questions on Google.")
+
+st.header('AI SMASH')
+
+for message in st.session_state.history:
+    role = message["role"]
+    content = message["content"]
+    st.chat_message(role).markdown(content)
+
+query = st.chat_input("Ask Anything !")
+if query:
+    st.chat_message("user").markdown(query)  
+    st.session_state.history.append({"role":"user","content":query})
     response = agent.invoke({"messages":[{"role":"user","content":query}]},
                         {"configurable":{"thread_id":"moon"}})
-    print(response["messages"][-1].content)
+    answer = response["messages"][-1].content
+    st.chat_message("ai").markdown(answer)  
+    st.session_state.history.append({"role":"ai","content":answer})
